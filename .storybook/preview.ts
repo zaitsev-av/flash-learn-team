bc6c28b185c5a910f2dff39c163ffa251b259414
Alexandr Zaitsev <zaitsev.av.12@gmail.com>
2023-07-06 14:06:16 +0300

    feat: added edit mode for user name.
    added dark mode for storybook.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,9 +2,16 @@ import type { Preview } from '@storybook/react'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import '../src/styles/index.scss'
-
+import { themes } from '@storybook/theming'
 const preview: Preview = {
   parameters: {
+    darkMode: {
+      dark: { ...themes.dark, appBg: '#171717' },
+      light: { ...themes.normal, appBg: '#808080' },
+    },
+    docs: {
+      theme: themes.dark,
+    },
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
