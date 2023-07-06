0e731658841004d5e5a4489cba32acd44235efaa
jalxby <jalxby@gmail.com>
2023-07-06 15:36:58 +0200

    fix: HeadCell & DataCell added

--- src/components/ui/table/table.stories.tsx
+++ src/components/ui/table/table.stories.tsx
@@ -1,17 +1,86 @@
 import { Meta, StoryObj } from '@storybook/react'
 
-import { Table } from '@/components'
+import s from './table.module.scss'
 
+import { Checkbox, DataCell, HeadCell, Table, TableBody, TableHead, TableRow } from '@/components'
 const meta = {
   title: 'Components/Table',
   component: Table,
   tags: ['autodocs'],
+  render: args => <Table {...args} />,
 } satisfies Meta<typeof Table>
 
 export default meta
 type Story = StoryObj<typeof meta>
+export const Default: Story = {
+  args: {
+    children: (
+      <>
+        <TableHead>
+          <TableRow>
+            <HeadCell title={'Name'} />
+            <HeadCell title={'Cards'} />
+            <HeadCell title={'Last Updated'} />
+            <HeadCell title={'Created by'} />
+            <HeadCell />
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <DataCell>{'Test1'}</DataCell>
+            <DataCell>{'Test2'}</DataCell>
+            <DataCell>{'Test3'}</DataCell>
+            <DataCell>{'Test4'}</DataCell>
+            <DataCell>{'Test5'}</DataCell>
+          </TableRow>
+          <TableRow>
+            <DataCell>{'Test1'}</DataCell>
+            <DataCell>{'Test2'}</DataCell>
+            <DataCell>{'Test3'}</DataCell>
+            <DataCell>{'Test4'}</DataCell>
+            <DataCell>{'Test5'}</DataCell>
+          </TableRow>
+        </TableBody>
+      </>
+    ),
+  },
+}
 
-export const Select_default: Story = {
-  args: {},
-  render: args => <Table {...args} />,
+export const TableWithReactNode: Story = {
+  args: {
+    children: (
+      <>
+        <TableHead>
+          <TableRow>
+            <HeadCell title={'Name'} />
+            <HeadCell title={'Cards'} />
+            <HeadCell title={'Last Updated'} />
+            <HeadCell title={'Created by'} />
+            <HeadCell />
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <DataCell>{'Test'}</DataCell>
+            <DataCell>{'Test2'}</DataCell>
+            <DataCell>{'Test3'}</DataCell>
+            <DataCell>{<Checkbox className={s.checkbox} label={'Name'} />}</DataCell>
+            <DataCell>{'Test3'}</DataCell>
+          </TableRow>
+        </TableBody>
+      </>
+    ),
+  },
+}
+
+export const Head_Cell: Story = {
+  args: {
+    children: <HeadCell title={'Name'} />,
+  },
+}
+
+export const Data_Cell: Story = {
+  args: {
+    children: <DataCell>{'Name'}</DataCell>,
+  },
 }
--- src/components/ui/table/table.tsx
+++ src/components/ui/table/table.tsx
@@ -1,10 +1,17 @@
 import { ComponentProps, FC } from 'react'
 
+import { clsx } from 'clsx'
+
+import s from './table.module.scss'
+
 import { Typography } from '@/components'
 
 type TableProps = { className?: string } & ComponentProps<'table'>
+
 export const Table: FC<TableProps> = ({ className, ...rest }) => {
-  return <table className={className} {...rest} />
+  const style = clsx(s.table, className)
+
+  return <table className={style} {...rest} />
 }
 
 type HeadProps = { className?: string } & ComponentProps<'thead'>
@@ -22,16 +29,28 @@ export const TableRow: FC<RowProps> = ({ className, ...rest }) => {
   return <tr className={className} {...rest} />
 }
 
-type HeadCellProps = { className?: string; title: string } & ComponentProps<'th'>
+type HeadCellProps = {
+  minWidth?: number
+  className?: string
+  title?: string
+} & ComponentProps<'th'>
 export const HeadCell: FC<HeadCellProps> = ({ title, className, ...rest }) => {
+  const style = clsx(s.headCell, className)
+
   return (
-    <th className={className} {...rest}>
+    <th className={style} {...rest}>
       <Typography variant={'subtitle2'}>{title}</Typography>
     </th>
   )
 }
 
 type DataCellProps = { className?: string } & ComponentProps<'td'>
-export const DataCell: FC<DataCellProps> = ({ className, ...rest }) => {
-  return <td className={className} {...rest} />
+export const DataCell: FC<DataCellProps> = ({ children, content, className, ...rest }) => {
+  const style = clsx(s.dataCell, className)
+
+  return (
+    <td className={style} {...rest}>
+      <div className={s.dataCellChildren}>{children}</div>
+    </td>
+  )
 }
