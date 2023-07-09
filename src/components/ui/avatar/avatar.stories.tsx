d80b118a5a5ac7e0c45103f1eca1c96f458221b3
jalxby <jalxby@gmail.com>
2023-07-10 00:44:47 +0200

    fix: avatar resizeable

--- src/components/ui/avatar/avatar.stories.tsx
+++ src/components/ui/avatar/avatar.stories.tsx
@@ -6,15 +6,20 @@ const meta = {
   title: 'Components/Avatar',
   component: Avatar,
   tags: ['autodocs'],
-  argTypes: {},
+  argTypes: { size: { type: 'number' } },
 } satisfies Meta<typeof Avatar>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {
+export const With_Image: Story = {
   args: {
     label: 'User Name',
     src: 'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80',
+  },
+}
+export const No_Image: Story = {
+  args: {
+    label: 'User Name',
   },
 }
--- src/components/ui/avatar/avatar.tsx
+++ src/components/ui/avatar/avatar.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { ComponentProps, FC } from 'react'
 
 import * as AvatarRDX from '@radix-ui/react-avatar'
 import * as Label from '@radix-ui/react-label'
@@ -9,23 +9,38 @@ import s from './avatar.module.scss'
 export type UserAvatarPropsType = {
   className?: string
   label?: string
-  src?: string
+  src?: ComponentProps<'img'>['src']
+  size?: ComponentProps<'img'>['width']
 }
-export const Avatar: FC<UserAvatarPropsType> = ({ label, className, src }) => {
+export const Avatar: FC<UserAvatarPropsType> = ({ size = 36, label, className, src }) => {
+  const fallbackText = label?.slice(0, 2).toUpperCase()
+  const fallbackSize = { width: `${size}px`, height: `${size}px` }
   const classNames = {
-    wrapper: clsx(s.wrapper, className),
-    root: clsx(s.root),
+    wrapper: clsx(s.wrapper),
+    root: clsx(s.root, className),
+    avatar: clsx(s.avatar_image),
+    fallback: clsx(s.avatarFallback),
+    label: clsx(s.label_wrapper),
   }
 
   return (
     <div className={classNames.wrapper}>
       {label && (
-        <Label.Root className={s.label_wrapper} htmlFor="user name">
+        <Label.Root className={classNames.label} htmlFor="user name">
           {label}
         </Label.Root>
       )}
       <AvatarRDX.Root className={classNames.root}>
-        <AvatarRDX.Image className={s.avatar_image} src={src} alt="User avatar" />
+        <AvatarRDX.Image
+          className={classNames.avatar}
+          width={size}
+          height={size}
+          src={src}
+          alt="User avatar"
+        />
+        <AvatarRDX.Fallback style={fallbackSize} className={classNames.fallback} delayMs={600}>
+          {fallbackText}
+        </AvatarRDX.Fallback>
       </AvatarRDX.Root>
     </div>
   )
