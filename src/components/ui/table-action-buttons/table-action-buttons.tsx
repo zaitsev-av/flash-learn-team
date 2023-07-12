b74cf7f99d7fc1be6bf75ce0e50f2e70ffab7055
jalxby <jalxby@gmail.com>
2023-07-13 00:57:45 +0200

    feat: delete dialog for packs and cards

--- src/components/ui/table-action-buttons/table-action-buttons.tsx
+++ src/components/ui/table-action-buttons/table-action-buttons.tsx
@@ -4,11 +4,13 @@ import s from './table-action-buttons.module.scss'
 
 import { EditIcon, PlayIcon, DeleteIcon } from '@/assets'
 import { AddNewPackModal } from '@/components/ui/modal/add-new-pack-modal'
+import { DeleteDialog, ItemType } from '@/components/ui/modal/delete-dialog/delete-dialog.tsx'
 
 type TableActionsProps = {
   editable?: boolean
+  item: ItemType
 }
-export const TableActions: FC<TableActionsProps> = ({ editable = true }) => {
+export const TableActions: FC<TableActionsProps> = ({ item, editable = true }) => {
   return (
     <div className={s.container}>
       <AddNewPackModal
@@ -29,13 +31,18 @@ export const TableActions: FC<TableActionsProps> = ({ editable = true }) => {
             }
             onSubmit={() => {}}
           />
-          <AddNewPackModal
+          <DeleteDialog
             trigger={
               <button>
                 <DeleteIcon />
               </button>
             }
-            onSubmit={() => {}}
+            buttonTitle={'Delete Pack'}
+            item={item}
+            onClick={id => {
+              console.log(id)
+            }}
+            title={'Delete Pack'}
           />
         </>
       )}
