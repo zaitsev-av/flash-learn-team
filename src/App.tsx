43b3170a30c1902af2eb762f98ad56199b04aeec
jalxby <jalxby@gmail.com>
2023-07-13 17:54:21 +0200

    feat: edit-pack-modal dialog, refactoring add-new-pack-modal according new modals

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from 'react'
-
-import { Button, Card, Checkbox, Pagination, Select, TextField } from '@/components'
-import { Modal } from '@/components/ui/modal'
+import { Button, Card, Checkbox, Pagination, Select } from '@/components'
 
 export const App = () => {
-  const [isModalOpen, setModalOpen] = useState(false)
-
   return (
     <>
       <Button />
@@ -16,37 +11,7 @@ export const App = () => {
           <Select fullWidth items={['select1', 'select13', 'select12', 'select11']} />
         </Card>
       </div>
-      <Modal
-        isOpen={isModalOpen}
-        onOpenChange={setModalOpen}
-        body={
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              gap: '1.5rem',
-            }}
-          >
-            <Select
-              label={'Choose a question format'}
-              items={['select1', 'select1', 'select1', 'select1']}
-            />
-            <TextField inputType={'text'} title={'Question'} />
-            <TextField inputType={'text'} title={'Answer'} />
-          </div>
-        }
-        footer={[
-          <Button key={1} variant={'secondary'} onClick={() => setModalOpen(false)}>
-            Cancel
-          </Button>,
-          <Button key={2} variant={'primary'}>
-            Add new card
-          </Button>,
-        ]}
-        trigger={<Button variant={'primary'}>modal</Button>}
-        title={'Title'}
-      />
+
       <div style={{ paddingLeft: '35%' }}>
         <Checkbox />
         <Select
---
+++ src/components/ui/modal/edit-pack-modal/edit-pack-modal.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { EditIcon } from '@/assets'
+import { EditPackModal } from '@/components/ui/modal/edit-pack-modal/edit-pack-modal.tsx'
+
+const meta = {
+  title: 'Modals/Edit Pack Modal',
+  component: EditPackModal,
+  tags: ['autodocs'],
+} satisfies Meta<typeof EditPackModal>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    packName: 'EditedPackName',
+    isPrivate: true,
+    trigger: <EditIcon />,
+    onSubmit: data => {
+      // eslint-disable-next-line no-console
+      console.log(data)
+    },
+  },
+}
