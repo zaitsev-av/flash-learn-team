0415801d7b4f0b1d242bdfe4357af629aa48abd5
jalxby <jalxby@gmail.com>
2023-07-15 01:54:38 +0200

    feat: Pack List Page

---
+++ src/components/packs/packs.tsx
@@ -0,0 +1,73 @@
+import { FC, useState } from 'react'
+
+import { clsx } from 'clsx'
+
+import s from './packs.module.scss'
+
+import { Logo } from '@/assets'
+import { AddNewPackModal, Button, Header, Pagination, Sort, Table, Typography } from '@/components'
+import { FilterPanel } from '@/components/ui/filter-panel'
+import { columns, data } from '@/components/ui/table/table.stories.tsx'
+import { TableActions } from '@/components/ui/table-action-buttons'
+
+type PacksProps = {}
+export const Packs: FC<PacksProps> = () => {
+  //TODO all this states will be replaced with backend
+  const [page, setPage] = useState<number>(1)
+  const [pageSize, setPageSize] = useState<string>('7')
+  const [sort, setSort] = useState<Sort>(null)
+  const cNames = {
+    container: clsx(s.container, 'container'),
+    title: clsx(s.pageTitle),
+  }
+
+  return (
+    <>
+      <Header>
+        <Logo />
+        <Button>Sign In</Button>
+      </Header>
+      <div className={cNames.container}>
+        <div className={cNames.title}>
+          <Typography variant={'large'}>Pack list</Typography>
+          <AddNewPackModal
+            trigger={<Button>Add New Pack</Button>}
+            onSubmit={data => {
+              console.log(data)
+            }}
+          />
+        </div>
+
+        <FilterPanel />
+
+        <Table.Root>
+          <Table.Head sort={sort} onSort={setSort} columns={columns}></Table.Head>
+          <Table.Body>
+            {data.slice(0, +pageSize).map(row => {
+              return (
+                <Table.Row key={row.title}>
+                  <Table.DataCell>{row.title}</Table.DataCell>
+                  <Table.DataCell>{row.cardsCount}</Table.DataCell>
+                  <Table.DataCell>{row.updated}</Table.DataCell>
+                  <Table.DataCell>{row.createdBy}</Table.DataCell>
+                  <Table.DataCell>
+                    <TableActions editable={row.editable} item={row} />
+                  </Table.DataCell>
+                </Table.Row>
+              )
+            })}
+          </Table.Body>
+        </Table.Root>
+
+        <Pagination
+          currentPage={page}
+          totalCount={400}
+          pageSize={+pageSize}
+          siblingCount={3}
+          onPageChange={setPage}
+          onPageSizeChange={setPageSize}
+        />
+      </div>
+    </>
+  )
+}
--- src/components/ui/table/table.stories.tsx
+++ src/components/ui/table/table.stories.tsx
@@ -8,7 +8,127 @@ import s from './table.module.scss'
 import { Checkbox, Grade, Sort, Table } from '@/components'
 import { TableActions } from '@/components/ui/table-action-buttons'
 
-const data = [
+export const data = [
+  {
+    id: uuidv4(),
+    title: 'Project A',
+    cardsCount: 10,
+    updated: '2023-07-07',
+    createdBy: 'John Doe',
+    editable: false,
+  },
+  {
+    id: uuidv4(),
+    title: 'Project B',
+    cardsCount: 5,
+    updated: '2023-07-06',
+    createdBy: 'Jane Smith',
+    editable: true,
+  },
+  {
+    id: uuidv4(),
+    title: 'Project C',
+    cardsCount: 8,
+    updated: '2023-07-05',
+    createdBy: 'Alice Johnson',
+    editable: false,
+  },
+  {
+    id: uuidv4(),
+    title: 'Project D',
+    cardsCount: 3,
+    updated: '2023-07-07',
+    createdBy: 'Bob Anderson',
+    editable: true,
+  },
+  {
+    id: uuidv4(),
+    title: 'Project E',
+    cardsCount: 12,
+    updated: '2023-07-04',
+    createdBy: 'Emma Davis',
+    editable: false,
+  },
+  {
+    id: uuidv4(),
+    title: 'Project A',
+    cardsCount: 10,
+    updated: '2023-07-07',
+    createdBy: 'John Doe',
+    editable: false,
+  },
+  {
+    id: uuidv4(),
+    title: 'Project B',
+    cardsCount: 5,
+    updated: '2023-07-06',
+    createdBy: 'Jane Smith',
+    editable: true,
+  },
+  {
+    id: uuidv4(),
+    title: 'Project C',
+    cardsCount: 8,
+    updated: '2023-07-05',
+    createdBy: 'Alice Johnson',
+    editable: false,
+  },
+  {
+    id: uuidv4(),
+    title: 'Project D',
+    cardsCount: 3,
+    updated: '2023-07-07',
+    createdBy: 'Bob Anderson',
+    editable: true,
+  },
+  {
+    id: uuidv4(),
+    title: 'Project E',
+    cardsCount: 12,
+    updated: '2023-07-04',
+    createdBy: 'Emma Davis',
+    editable: false,
+  },
+  {
+    id: uuidv4(),
+    title: 'Project A',
+    cardsCount: 10,
+    updated: '2023-07-07',
+    createdBy: 'John Doe',
+    editable: false,
+  },
+  {
+    id: uuidv4(),
+    title: 'Project B',
+    cardsCount: 5,
+    updated: '2023-07-06',
+    createdBy: 'Jane Smith',
+    editable: true,
+  },
+  {
+    id: uuidv4(),
+    title: 'Project C',
+    cardsCount: 8,
+    updated: '2023-07-05',
+    createdBy: 'Alice Johnson',
+    editable: false,
+  },
+  {
+    id: uuidv4(),
+    title: 'Project D',
+    cardsCount: 3,
+    updated: '2023-07-07',
+    createdBy: 'Bob Anderson',
+    editable: true,
+  },
+  {
+    id: uuidv4(),
+    title: 'Project E',
+    cardsCount: 12,
+    updated: '2023-07-04',
+    createdBy: 'Emma Davis',
+    editable: false,
+  },
   {
     id: uuidv4(),
     title: 'Project A',
@@ -51,7 +171,7 @@ const data = [
   },
 ]
 
-const columns = [
+export const columns = [
   {
     key: 'name',
     title: 'Name',
