3c4f7398f8eb1d6d2a681464c0fe5e2726415162
jalxby <jalxby@gmail.com>
2023-07-15 14:04:23 +0200

    fix: Import paths in RadioGroup stories

--- src/components/ui/radio-group/radio-group.stories.tsx
+++ src/components/ui/radio-group/radio-group.stories.tsx
@@ -3,7 +3,9 @@ import { useState } from 'react'
 import type { Meta } from '@storybook/react'
 import { v4 } from 'uuid'
 
-import { RadioGroupItemType, RadioGroup } from '@/components'
+import { RadioGroupItemType } from './radio-group.tsx'
+
+import { RadioGroup } from '@/components'
 
 const items: RadioGroupItemType[] = [
   { id: v4(), label: 'label1', value: 'value1' },
