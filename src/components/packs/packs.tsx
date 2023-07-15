40ed816c7500315968350b0c2ef6414ceaf709aa
jalxby <jalxby@gmail.com>
2023-07-15 03:46:07 +0200

    fix: Refactor table rendering in Packs component
    The code for creating table rows in the Packs component was extracted into its own constant for better readability and separation of concerns. The import of the 'clsx' library was also corrected.

--- src/components/packs/packs.tsx
+++ src/components/packs/packs.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from 'react'
 
-import { clsx } from 'clsx'
+import clsx from 'clsx'
 
 import s from './packs.module.scss'
 
@@ -12,14 +12,26 @@ import { TableActions } from '@/components/ui/table-action-buttons'
 
 type PacksProps = {}
 export const Packs: FC<PacksProps> = () => {
-  //TODO all this states will be replaced with backend
   const [page, setPage] = useState<number>(1)
   const [pageSize, setPageSize] = useState<string>('7')
   const [sort, setSort] = useState<Sort>(null)
+
   const cNames = {
     container: clsx(s.container, 'container'),
     title: clsx(s.pageTitle),
   }
+
+  const tableRows = data.slice(0, +pageSize).map(row => (
+    <Table.Row key={row.title}>
+      <Table.DataCell>{row.title}</Table.DataCell>
+      <Table.DataCell>{row.cardsCount}</Table.DataCell>
+      <Table.DataCell>{row.updated}</Table.DataCell>
+      <Table.DataCell>{row.createdBy}</Table.DataCell>
+      <Table.DataCell>
+        <TableActions editable={row.editable} item={row} />
+      </Table.DataCell>
+    </Table.Row>
+  ))
 
   return (
     <>
@@ -42,21 +54,7 @@ export const Packs: FC<PacksProps> = () => {
 
         <Table.Root>
           <Table.Head sort={sort} onSort={setSort} columns={columns}></Table.Head>
-          <Table.Body>
-            {data.slice(0, +pageSize).map(row => {
-              return (
-                <Table.Row key={row.title}>
-                  <Table.DataCell>{row.title}</Table.DataCell>
-                  <Table.DataCell>{row.cardsCount}</Table.DataCell>
-                  <Table.DataCell>{row.updated}</Table.DataCell>
-                  <Table.DataCell>{row.createdBy}</Table.DataCell>
-                  <Table.DataCell>
-                    <TableActions editable={row.editable} item={row} />
-                  </Table.DataCell>
-                </Table.Row>
-              )
-            })}
-          </Table.Body>
+          <Table.Body>{tableRows}</Table.Body>
         </Table.Root>
 
         <Pagination
