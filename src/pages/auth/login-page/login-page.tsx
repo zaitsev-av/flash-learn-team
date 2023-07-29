55ffc7ed46658d41ea63b597c7173c0ba7bcb971
Alexandr Zaitsev <zaitsev.av.12@gmail.com>
2023-07-29 21:58:15 +0300

    fix: redirect to the main page after logging into the application.

--- src/pages/auth/login-page/login-page.tsx
+++ src/pages/auth/login-page/login-page.tsx
@@ -1,25 +1,31 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 
 import { LoginForm } from '@/components/auth'
 import { Page } from '@/components/ui/page'
-import { useLoginMutation } from '@/services/auth'
+import { useAuthMeQuery, useLoginMutation } from '@/services/auth'
 
 export const LoginPage: FC = () => {
   const [login] = useLoginMutation()
+  const { data } = useAuthMeQuery()
   const navigate = useNavigate()
   const handleLogin = async (args: any) => {
     try {
       await login(args).unwrap()
       toast.success('Вы успешно авторизовались!')
       console.log('navigate')
-      navigate('/')
     } catch (err) {
       return console.log(err)
     }
   }
+
+  useEffect(() => {
+    if (!data) return
+
+    navigate('/')
+  }, [data])
 
   return (
     <Page>
