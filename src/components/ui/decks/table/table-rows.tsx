fb2d915bbf4e6e53b697f1d734cd781c11da213c
Alexandr Zaitsev <zaitsev.av.12@gmail.com>
2023-08-17 18:44:01 +0300

    fix: ui.

--- src/components/ui/decks/table/table-rows.tsx
+++ src/components/ui/decks/table/table-rows.tsx
@@ -2,6 +2,8 @@ import { FC, useState } from 'react'
 
 import { useNavigate } from 'react-router-dom'
 
+import s from '../decks.module.scss'
+
 import { transformDate } from '@/common'
 import { Table } from '@/components'
 import { TableActions } from '@/components/ui/table-action-buttons'
@@ -24,8 +26,11 @@ export const TableRows: FC<TableRowsPropsType> = props => {
     <>
       {data.slice(0, pageSize).map(el => (
         <Table.Row key={el.id}>
-          <Table.DataCell onClick={() => navigate(`/cards/${el.id}`)} style={{ cursor: 'pointer' }}>
-            <span style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+          <Table.DataCell onClick={() => navigate(`/cards/${el.id}`)}>
+            <span
+              style={{ display: 'flex', alignItems: 'center', gap: '10px' }}
+              className={s.getCards}
+            >
               {el.name}
               {el.cover === null ? '' : <img src={el.cover} alt="" width="70px" height="50px" />}
             </span>
