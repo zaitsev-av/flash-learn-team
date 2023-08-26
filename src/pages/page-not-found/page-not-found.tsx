9d5f67902b4656f3a4fbf522b3cb421ceb271fe1
Alexandr Zaitsev <zaitsev.av.12@gmail.com>
2023-08-26 20:39:11 +0300

    feat: added page-not-fount

--- src/pages/page-not-found/page-not-found.tsx
+++ src/pages/page-not-found/page-not-found.tsx
@@ -1,8 +1,7 @@
 import { FC } from 'react'
 
 import { clsx } from 'clsx'
-//todo uncomment
-//import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 import s from './page-not-found.module.scss'
 
@@ -10,8 +9,7 @@ import { Page404 } from '@/assets'
 import { Button, Typography } from '@/components'
 
 export const PageNotFound: FC = () => {
-  //todo указать путь
-  // const navigate = useNavigate()
+  const navigate = useNavigate()
   const classNames = {
     wrapper: clsx(s.wrapper),
     container: clsx(s.container),
@@ -26,12 +24,7 @@ export const PageNotFound: FC = () => {
         <Typography variant={'body1'} className={classNames.message}>
           Sorry! Page not found!
         </Typography>
-        <Button
-          variant={'primary'}
-          className={classNames.btn}
-          //todo uncomment
-          // onClick={() => navigate('PATH')}
-        >
+        <Button variant={'primary'} className={classNames.btn} onClick={() => navigate('/')}>
           Back to home page
         </Button>
       </div>
