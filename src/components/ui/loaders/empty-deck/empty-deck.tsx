55688a192c8440d2a5478b44344a40e544be3336
Alexandr Zaitsev <zaitsev.av.12@gmail.com>
2023-08-27 20:44:08 +0300

    feat: animation for background on empty-deck page

--- src/components/ui/loaders/empty-deck/empty-deck.tsx
+++ src/components/ui/loaders/empty-deck/empty-deck.tsx
@@ -1,60 +1,55 @@
 import { FC } from 'react'
 
+import { clsx } from 'clsx'
+import { useNavigate } from 'react-router-dom'
+
 import s from './empty-deck.module.scss'
 
-import { Typography } from '@/components'
+import { ArrowLeftIcon } from '@/assets'
+import { Button, Typography } from '@/components'
 
 export const EmptyDeck: FC = () => {
+  const navigate = useNavigate()
+
+  const classNames = {
+    back: clsx(s.back),
+    backArrow: clsx(s.content),
+  }
+
+  const navigateBack = () => {
+    navigate(-1)
+  }
+
   return (
-    <div className={s.wrapper}>
-      <div className={s.box_of_star1}>
-        <div className={`${s.star} ${s.star_position1}`} />
-        <div className={`${s.star} ${s.star_position2}`} />
-        <div className={`${s.star} ${s.star_position3}`} />
-        <div className={`${s.star} ${s.star_position4}`} />
-        <div className={`${s.star} ${s.star_position5}`} />
-        <div className={`${s.star} ${s.star_position6}`} />
-        <div className={`${s.star} ${s.star_position7}`} />
-      </div>
-      <Typography variant={'large'}>Oops, there are no cards in your chosen deck.</Typography>
-      <div className={s.box_of_star2}>
-        <div className={`${s.star} ${s.star_position1}`} />
-        <div className={`${s.star} ${s.star_position2}`} />
-        <div className={`${s.star} ${s.star_position3}`} />
-        <div className={`${s.star} ${s.star_position4}`} />
-        <div className={`${s.star} ${s.star_position5}`} />
-        <div className={`${s.star} ${s.star_position6}`} />
-        <div className={`${s.star} ${s.star_position7}`} />
-      </div>
-      <div className={s.box_of_star3}>
-        <div className={`${s.star} ${s.star_position1}`} />
-        <div className={`${s.star} ${s.star_position2}`} />
-        <div className={`${s.star} ${s.star_position3}`} />
-        <div className={`${s.star} ${s.star_position4}`} />
-        <div className={`${s.star} ${s.star_position5}`} />
-        <div className={`${s.star} ${s.star_position6}`} />
-        <div className={`${s.star} ${s.star_position7}`} />
-      </div>
-      <div className={s.box_of_star4}>
-        <div className={`${s.star} ${s.star_position1}`} />
-        <div className={`${s.star} ${s.star_position2}`} />
-        <div className={`${s.star} ${s.star_position3}`} />
-        <div className={`${s.star} ${s.star_position4}`} />
-        <div className={`${s.star} ${s.star_position5}`} />
-        <div className={`${s.star} ${s.star_position6}`} />
-        <div className={`${s.star} ${s.star_position7}`} />
-      </div>
-      <div data-js="astro" className={s.astronaut}>
-        <div className={s.head} />
-        <div className={`${s.arm} ${s.arm_left}`} />
-        <div className={`${s.arm} ${s.arm_right}`} />
-        <div className={s.body}>
-          <div className={s.panel} />
+    <>
+      <Button variant={'link'} onClick={navigateBack} className={classNames.back}>
+        <Typography variant={'body2'} className={classNames.backArrow}>
+          <ArrowLeftIcon /> Go back
+        </Typography>
+      </Button>
+      <div className={s.wrapper}>
+        <Typography variant={'large'}>Oops, there are no cards in your chosen deck.</Typography>
+
+        <div data-js="astro" className={s.astronaut}>
+          <div className={s.head} />
+          <div className={`${s.arm} ${s.arm_left}`} />
+          <div className={`${s.arm} ${s.arm_right}`} />
+          <div className={s.body}>
+            <div className={s.panel} />
+          </div>
+          <div className={`${s.leg} ${s.leg_left}`} />
+          <div className={`${s.leg} ${s.leg_right}`} />
+          <div className={s.schoolbag} />
         </div>
-        <div className={`${s.leg} ${s.leg_left}`} />
-        <div className={`${s.leg} ${s.leg_right}`} />
-        <div className={s.schoolbag} />
+        <div className={s.elements}>
+          <span className={s.el_1}></span>
+          <span className={s.el_2}></span>
+          <span className={s.el_3}></span>
+          <span className={s.el_4}></span>
+          <span className={s.el_5}></span>
+          <span className={s.el_6}></span>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
--- src/pages/learn-deck-page/learn-deck-page.tsx
+++ src/pages/learn-deck-page/learn-deck-page.tsx
@@ -5,14 +5,15 @@ import { useParams } from 'react-router-dom'
 import { Page } from '@/components'
 import { LearnDesk } from '@/components/ui/learn-deck'
 import { EmptyDeck } from '@/components/ui/loaders/empty-deck/empty-deck.tsx'
+import { InitialLoader } from '@/components/ui/loaders/initial-loader/initial-loader.tsx'
 import { useGetDeckQuery, useLearnDeckQuery, useUpdateCardGradeMutation } from '@/services'
 
 export const LearnDeckPage: FC = () => {
   const [value, setValue] = useState<string>('')
   const { id } = useParams<{ id: string }>()
-  const { data: cardData } = useLearnDeckQuery(id ?? '')
+  const { data: cardData, isLoading: cardIsLoading } = useLearnDeckQuery(id ?? '')
   const [updateCardGrade] = useUpdateCardGradeMutation()
-  const { data: deckData } = useGetDeckQuery(id ?? '')
+  const { data: deckData, isLoading: deckIsLoading } = useGetDeckQuery(id ?? '')
 
   const handleUpdateCardGrade = () => {
     if (id && cardData) {
@@ -21,14 +22,17 @@ export const LearnDeckPage: FC = () => {
     }
   }
 
-  if (cardData === undefined)
+  console.log(cardIsLoading)
+  if (cardIsLoading && deckIsLoading) return <InitialLoader />
+
+  console.log(cardData)
+  if (cardData === undefined) {
     return (
       <Page>
         <EmptyDeck />
       </Page>
     )
-
-  console.log(cardData)
+  }
 
   return (
     <Page>
